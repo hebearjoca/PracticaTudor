1cea05a2c2bb660d4c7c7f86da711f11e653113e
Hebe Arjoca <hebearjoca@yahoo.com>
2021-03-29 18:08:05 +0300

    modificari

---
+++ WindowsFormsApp1/WindowsFormsApp1/MainForm.Designer.cs
@@ -0,0 +1,93 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.toolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.grupa1ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.grupa2ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripMenuItem1,
+            this.grupa1ToolStripMenuItem,
+            this.grupa2ToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(800, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // toolStripMenuItem1
+            // 
+            this.toolStripMenuItem1.Enabled = false;
+            this.toolStripMenuItem1.Name = "toolStripMenuItem1";
+            this.toolStripMenuItem1.Size = new System.Drawing.Size(125, 20);
+            this.toolStripMenuItem1.Text = "toolStripMenuItem1";
+            this.toolStripMenuItem1.Visible = false;
+            // 
+            // grupa1ToolStripMenuItem
+            // 
+            this.grupa1ToolStripMenuItem.Name = "grupa1ToolStripMenuItem";
+            this.grupa1ToolStripMenuItem.Size = new System.Drawing.Size(60, 20);
+            this.grupa1ToolStripMenuItem.Text = "Grupa 1";
+            // 
+            // grupa2ToolStripMenuItem
+            // 
+            this.grupa2ToolStripMenuItem.Name = "grupa2ToolStripMenuItem";
+            this.grupa2ToolStripMenuItem.Size = new System.Drawing.Size(60, 20);
+            this.grupa2ToolStripMenuItem.Text = "Grupa 2";
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.menuStrip1);
+            this.Name = "MainForm";
+            this.Text = "Form1";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem grupa1ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem grupa2ToolStripMenuItem;
+    }
+}
+
